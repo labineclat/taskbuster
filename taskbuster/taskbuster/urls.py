2d7ae118122ac4d6bab2648bc5da469f68406769
djangoineclat <django.ineclat@gmail.com>
2015-05-27 00:10:28 +0530

    Settings, static files and templates

--- taskbuster/taskbuster/urls.py
+++ taskbuster/taskbuster/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from .views import home
+
 
 urlpatterns = [
     # Examples:
@@ -7,4 +9,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', home, name='home'),
 ]
