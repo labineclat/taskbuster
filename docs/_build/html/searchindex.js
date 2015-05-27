5f9f96cc5f0491a209f182e6241eeb0af03a05ce
djangoineclat <django.ineclat@gmail.com>
2015-05-27 22:14:18 +0530

    Sphinx installed and configured 2

--- docs/_build/html/searchindex.js
+++ docs/_build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to taskBuster&#8217;s documentation!"],titleterms:{document:0,indic:0,tabl:0,taskbust:0,welcom:0}})
+Search.setIndex({envversion:47,filenames:["index","quick_start","requirements"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to taskBuster&#8217;s documentation!","&lt;no title&gt;","&lt;no title&gt;"],titleterms:{document:0,indic:0,tabl:0,taskbust:0,welcom:0}})
